ae8a86107798310cb5280377e1634bcd20ddad9e
woozziam <leejungwook0211@gmail.com>
2024-05-23 12:20:01 +0900

    upload face_recognition communicate with ROS ver.1

---
+++ src/user_recognition/FaceRecognition.py
@@ -0,0 +1,102 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import cv2
+import face_recognition
+
+class FaceRecognition:
+    def __init__(self):
+        self.known_face_encodings = []
+        self.known_face_names = []
+        self.known_face_locations = []
+
+    def plt_imshow(self, title='image', img=None, figsize=(8, 5)):
+        if isinstance(img, np.ndarray):
+            if len(img.shape) < 3:
+                rgbImg = cv2.cvtColor(img, cv2.COLOR_GRAY2RGB)
+            else:
+                rgbImg = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+
+            plt.imshow(rgbImg)
+            plt.title(title)
+            plt.xticks([]), plt.yticks([])
+            plt.show()
+
+    def name_labeling(self, input_image, show_result=True):
+        image = input_image.copy()
+        face_locations = face_recognition.face_locations(image)
+        face_encodings = face_recognition.face_encodings(image, face_locations)
+
+        face_names = []
+
+        for face_encoding in face_encodings:
+            matches = face_recognition.compare_faces(self.known_face_encodings, face_encoding, tolerance=0.5)
+            name = "Unknown"
+
+            face_distances = face_recognition.face_distance(self.known_face_encodings, face_encoding)
+            best_match_index = np.argmin(face_distances)
+
+            if matches[best_match_index]:
+                name = self.known_face_names[best_match_index]
+
+            face_names.append(name)
+
+        for (top, right, bottom, left), name in zip(face_locations, face_names):
+            if name != "Unknown":
+                color = (0, 255, 0)
+            else:
+                color = (0, 0, 255)
+
+            cv2.rectangle(image, (left, top), (right, bottom), color, 1)
+            cv2.rectangle(image, (left, bottom - 10), (right, bottom), color, cv2.FILLED)
+            font = cv2.FONT_HERSHEY_DUPLEX
+            cv2.putText(image, name, (left + 3, bottom - 3), font, 0.2, (0, 0, 0), 1)
+
+        if show_result:
+            self.plt_imshow("Output", image, figsize=(24, 15))
+
+        return image, face_names
+
+    def draw_label(self, input_image, coordinates, label):
+        image = input_image.copy()
+        (top, right, bottom, left) = coordinates
+        cv2.rectangle(image, (left, top), (right, bottom), (0, 255, 0), 5)
+        cv2.putText(image, label, (left - 10, top - 10), cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 255, 0), 3)
+
+        return image
+
+    def add_known_face(self, face_image_path, name):
+        face_image = cv2.imread(face_image_path)
+        face_location = face_recognition.face_locations(face_image)[0]
+        face_encoding = face_recognition.face_encodings(face_image)[0]
+
+        detected_face_image = self.draw_label(face_image, face_location, name)
+
+        self.known_face_encodings.append(face_encoding)
+        self.known_face_names.append(name)
+        self.known_face_locations.append(face_location)
+
+        # self.plt_imshow("Detected Face", detected_face_image)
+
+if __name__ == "__main__":
+    face_recog = FaceRecognition()
+
+    face_recog.add_known_face("wooks/wook_0.jpg", "wook")
+    face_recog.add_known_face("kyus/kyu_0.jpg", "kyu")
+
+    cap = cv2.VideoCapture(0)
+
+    while True:
+        ret, frame = cap.read()
+        if not ret:
+            print("웹캠에서 영상을 불러올 수 없습니다.")
+            break
+
+        labeled_frame = face_recog.name_labeling(frame, show_result=False)
+
+        cv2.imshow('Webcam', labeled_frame)
+
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    cap.release()
+    cv2.destroyAllWindows()
