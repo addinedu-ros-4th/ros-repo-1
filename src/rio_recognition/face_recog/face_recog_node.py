0c5b95a29ecc284aba940516ed6432d2fc703f7a
woozziam <leejungwook0211@gmail.com>
2024-05-29 09:53:55 +0900

    add ROS service that publish face_landmarks to register user's face

--- src/rio_recognition/face_recog/face_recog_node.py
+++ src/rio_recognition/face_recog/face_recog_node.py
@@ -2,46 +2,67 @@ import rclpy as rp
 from rclpy.node import Node
 from sensor_msgs.msg import Image
 from std_msgs.msg import String
+from std_srvs.srv import SetBool
 import cv2
 from FaceRecognition import FaceRecognition
 from cv_bridge import CvBridge
+
 bridge = CvBridge()
 
 class ImageSubscriber(Node):
     def __init__(self):
-        self.facerecognition = FaceRecognition()
         super().__init__('image_sub')
+        self.facerecognition = FaceRecognition()
         self.image_sub = self.create_subscription(
             Image,
             '/image_raw',
             self.image_callback,
             10
         )
-        self.face_names_pub = self.create_publisher(String, '/face_names', 10)
 
-    def image_callback(self, data) :
-        cv_image = bridge.imgmsg_to_cv2(data, 'bgr8')
-        labeled_image, face_names = self.facerecognition.name_labeling(cv_image, show_result=False)
+        self.face_names_pub = self.create_publisher(String, '/face_names', 10)
+        self.face_landmarks_pub = self.create_publisher(String, '/face_landmarks', 10)
+
+        self.srv = self.create_service(SetBool, 'register_service', self.handle_register_service)
+
+    def image_callback(self, data):
+        self.cv_image = bridge.imgmsg_to_cv2(data, 'bgr8')
+        labeled_image, face_names = self.facerecognition.name_labeling(self.cv_image, show_result=False)
         cv2.imshow('face_recognition', labeled_image)
         cv2.waitKey(33)
 
-        # 얼굴 이름을 문자열로 변환하여 발행
         face_names_str = ','.join(face_names)
         self.face_names_pub.publish(String(data=face_names_str))
 
-def main(args=None) :
+    def handle_register_service(self, request, response):
+        if request.data:
+            latest_image = self.cv_image
+            self.publish_landmark(latest_image)
+            response.success = True
+            response.message = "Landmarks published"
+        else:
+            response.success = False
+            response.message = "No action taken"
+        return response
+
+    def publish_landmark(self, image):
+        face_landmarks = self.facerecognition.get_landmark(image)
+        face_landmarks_str = str(face_landmarks)
+        self.face_landmarks_pub.publish(String(data=face_landmarks_str))
+
+def main(args=None):
     rp.init(args=args)
     node = ImageSubscriber()
 
     node.facerecognition.add_known_face("wooks/wook_0.jpg", "wook")
 
-    try :
+    try:
         rp.spin(node)
-    except KeyboardInterrupt :
+    except KeyboardInterrupt:
         node.get_logger().info('KeyboardInterrupt')
-    finally :
+    finally:
         node.destroy_node()
         rp.shutdown()
 
-if __name__ == '__main__' :
+if __name__ == '__main__':
     main()
