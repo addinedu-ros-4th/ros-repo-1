55ece0dbb3bd6adecaed078059846dc6e6418307
gudxok <gudxok_@naver.com>
2024-06-10 18:05:45 +0900

    merge - build log gitignore add, log/install folder rm

--- src/rio_ui_msgs/install/rio_ui_msgs/share/rio_ui_msgs/cmake/rosidl_cmake-extras.cmake
+++
@@ -1,4 +0,0 @@
-# generated from rosidl_cmake/cmake/rosidl_cmake-extras.cmake.in
-
-set(rio_ui_msgs_IDL_FILES "srv/GenerateVisitQR.idl;srv/QRCheck.idl;srv/VisitorAlert.idl;msg/RobotCall.idl")
-set(rio_ui_msgs_INTERFACE_FILES "srv/GenerateVisitQR.srv;srv/GenerateVisitQR_Request.msg;srv/GenerateVisitQR_Response.msg;srv/QRCheck.srv;srv/QRCheck_Request.msg;srv/QRCheck_Response.msg;srv/VisitorAlert.srv;srv/VisitorAlert_Request.msg;srv/VisitorAlert_Response.msg;msg/RobotCall.msg")
